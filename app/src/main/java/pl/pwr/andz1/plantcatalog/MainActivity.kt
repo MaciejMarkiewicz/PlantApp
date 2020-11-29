c66f81c1c77010a34b69303ecd25e1f9a9d68f5b
Maciej <maciej.markiewicz@onet.pl>
2020-11-29 23:31:59 +0100

    finish list functionalities

--- app/src/main/java/pl/pwr/andz1/plantcatalog/MainActivity.kt
+++ app/src/main/java/pl/pwr/andz1/plantcatalog/MainActivity.kt
@@ -1,30 +1,119 @@
 package pl.pwr.andz1.plantcatalog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.*
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import pl.pwr.andz1.plantcatalog.plant.Plants
 import pl.pwr.andz1.plantcatalog.plant.PlantsAdapter
 
-class MainActivity : AppCompatActivity() {
+
+const val FAV_STATE: String = "FAV_STATE"
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var plantsAdapter: PlantsAdapter
+    private var favFilterState: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.tabanim_toolbar)
-        toolbar.title = resources.getString(R.string.app_name)
-        setSupportActionBar(toolbar)
+        favFilterState = savedInstanceState?.getBoolean(FAV_STATE, false) ?: false
 
         val recyclerView = findViewById<View>(R.id.recycler_view) as RecyclerView
-        val adapter = PlantsAdapter(Plants.ALL_PLANTS, this)
-        recyclerView.adapter = adapter
+        plantsAdapter = PlantsAdapter(Plants.plants, this)
+        recyclerView.adapter = plantsAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+        addSwipeDeleteAction(plantsAdapter, recyclerView)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val spinner: Spinner = findViewById(R.id.filter_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.catrgories,
+            R.layout.filter_menu_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.filter_menu_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(FAV_STATE, favFilterState)
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        plantsAdapter.addCategoryFilter(parent.getItemAtPosition(pos).toString())
+        handleNoPlants()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_filter, menu)
+
+        if (favFilterState) {
+            favFilterState = false
+            filterFavourites(menu?.findItem(R.id.only_fav)!!)
+        }
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.only_fav -> {
+                filterFavourites(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun filterFavourites(item: MenuItem) {
+        if (favFilterState) {
+            item.setIcon(R.drawable.ic_fav_empty)
+            plantsAdapter.removeFavFilter()
+        } else {
+            item.setIcon(R.drawable.ic_fav)
+            plantsAdapter.addFavFilter()
+        }
+
+        favFilterState = !favFilterState
+        handleNoPlants()
+    }
+
+    private fun addSwipeDeleteAction(adapter: PlantsAdapter, recyclerView: RecyclerView) {
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                v: RecyclerView, h: RecyclerView.ViewHolder,
+                t: RecyclerView.ViewHolder
+            ) = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, dir: Int) {
+                adapter.removeAt(viewHolder.adapterPosition)
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+                handleNoPlants()
+            }
+        }).attachToRecyclerView(recyclerView)
+    }
+
+    private fun handleNoPlants() {
+        if (plantsAdapter.isEmpty())
+            findViewById<TextView>(R.id.no_plants).visibility = View.VISIBLE
+        else
+            findViewById<TextView>(R.id.no_plants).visibility = View.GONE
     }
 }
+
--- app/src/main/java/pl/pwr/andz1/plantcatalog/plant/PlantsAdapter.kt
+++ app/src/main/java/pl/pwr/andz1/plantcatalog/plant/PlantsAdapter.kt
@@ -4,21 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pl.pwr.andz1.plantcatalog.R
 
-class PlantsAdapter(private val plants: List<Plants.Plant>, private val context: Context) :
+class PlantsAdapter(private val plants: MutableList<Plants.Plant>, private val context: Context) :
     RecyclerView.Adapter<PlantsAdapter.ViewHolder>() {
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
         val image: ImageView = itemView.findViewById(R.id.image_view)
         val favButton: ImageButton = itemView.findViewById(R.id.fav_button)
-        val title: TextView = itemView.findViewById(R.id.card_title)
-        val subtitle: TextView = itemView.findViewById(R.id.card_subtitle)
+        val title: TextView = itemView.findViewById(R.id.plant_name)
+        val category: TextView = itemView.findViewById(R.id.plant_category)
     }
+
+    var favFilterOn: Boolean = false
+        private set
+    private var categoryFilter: Plants.Category? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantsAdapter.ViewHolder {
         val context = parent.context
@@ -34,20 +37,102 @@ class PlantsAdapter(private val plants: List<Plants.Plant>, private val context:
 
         val titleView = viewHolder.title
         titleView.text = context.resources.getString(
-            getResourceId(name + "_full_name","string"))
+            getResourceId(name + "_full_name", "string")
+        )
 
-        val subtitleView = viewHolder.subtitle
-        subtitleView.text = context.resources.getString(
-            getResourceId(name + "_full_name","string"))
+        val categoryView = viewHolder.category
+        categoryView.text = getCategoryString(plant.category)
 
         val imageView = viewHolder.image
-        imageView.setImageResource(getResourceId("plants_${name}_1","drawable"))
+        imageView.setImageResource(getResourceId("plants_${name}_1", "drawable"))
+
+        val favButton = viewHolder.favButton
+        favButton.setOnClickListener {
+            plant.favourite = !plant.favourite
+            handleFavButton(plant, favButton)
+        }
+
+        handleFavButton(plant, favButton)
+    }
+
+    override fun getItemCount(): Int {
+        return plants.size
+    }
+
+    fun removeAt(position: Int) {
+        Plants.removePlant(position)
+    }
+
+    fun isEmpty(): Boolean {
+        return plants.isEmpty()
+    }
+
+    fun removeFavFilter() {
+        favFilterOn = false
+        filter()
+    }
+
+    fun addFavFilter() {
+        favFilterOn = true
+        filter()
+    }
+
+    fun addCategoryFilter(name: String) {
+        categoryFilter = getCategoryFromName(name)
+        filter()
+    }
+
+    private fun filter() {
+        val categoryPredicate: (Plants.Plant) -> Boolean = if (categoryFilter == null) {
+            { true }
+        } else {
+            { plant: Plants.Plant -> plant.category == categoryFilter }
+        }
+
+        val favPredicate: (Plants.Plant) -> Boolean = if (favFilterOn) {
+            { plant: Plants.Plant -> plant.favourite }
+        } else {
+            { true }
+        }
+
+        Plants.filterPlants { plant: Plants.Plant -> categoryPredicate(plant) && favPredicate(plant) }
+        notifyDataSetChanged()
+    }
+
+    private fun getCategoryString(category: Plants.Category): String {
+        val categoryResourceName: String = when (category) {
+            Plants.Category.POT_PLANT -> "pot_plant"
+            Plants.Category.GREEN -> "green"
+            Plants.Category.SUCCULENT -> "succulent"
+            Plants.Category.PALM -> "palm"
+        }
+
+        return context.resources.getString(
+            getResourceId("category_name_$categoryResourceName", "string")
+        )
+    }
+
+    private fun getCategoryFromName(name: String): Plants.Category? {
+        return when (name) {
+            "Pot plants" -> Plants.Category.POT_PLANT
+            "Green plants" -> Plants.Category.GREEN
+            "Succulents" -> Plants.Category.SUCCULENT
+            "Palms" -> Plants.Category.PALM
+            else -> return null
+        }
+    }
+
+    private fun handleFavButton(plant: Plants.Plant, favButton: ImageButton) {
+        if (plant.favourite) {
+            favButton.setImageResource(R.drawable.ic_fav)
+            favButton.isSelected = true
+        } else {
+            favButton.setImageResource(R.drawable.ic_fav_empty)
+            favButton.isSelected = false
+        }
     }
 
     private fun getResourceId(name: String, type: String): Int {
         return context.resources.getIdentifier(name, type, context.packageName)
-    }
-    override fun getItemCount(): Int {
-        return plants.size
     }
 }
