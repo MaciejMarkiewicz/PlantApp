e34a49ea60e24b1be575fe737f4b3d46ecceb9cb
Maciej <maciej.markiewicz@onet.pl>
2020-11-29 16:00:33 +0100

    finish list layout

--- app/src/main/java/pl/pwr/andz1/plantcatalog/MainActivity.kt
+++ app/src/main/java/pl/pwr/andz1/plantcatalog/MainActivity.kt
@@ -3,9 +3,12 @@ package pl.pwr.andz1.plantcatalog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import pl.pwr.andz1.plantcatalog.plant.Plants
 import pl.pwr.andz1.plantcatalog.plant.PlantsAdapter
 
@@ -14,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.tabanim_toolbar)
+        toolbar.title = resources.getString(R.string.app_name)
+        setSupportActionBar(toolbar)
 
         val recyclerView = findViewById<View>(R.id.recycler_view) as RecyclerView
         val adapter = PlantsAdapter(Plants.ALL_PLANTS, this)
